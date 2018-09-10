acc28ee8df59ee9be6a414c7a2dd91075564c340
BenjaminRasmussen <dumstick228@gmail.com>
2018-09-10 06:21:07 +0200

    added user controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906162037) do
+ActiveRecord::Schema.define(version: 20180910032453) do
 
   create_table "board_user_relations", force: :cascade do |t|
     t.integer "board_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180906162037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
